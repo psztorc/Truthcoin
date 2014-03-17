4c977d0e83ed5ab3404288f3afc814c017327d5b
psztorc <psztorc01@gmail.com>
2014-03-17 00:08:49 -0400

    pyTrading Finished
    Finished Python version of Trading

--- pylib/market/Trading.py
+++ pylib/market/Trading.py
@@ -69,8 +69,7 @@ def QueryMoveCost(ID,State,P):
 
 
 QueryMoveCost('Obama2012',1,.7)
-
-
+QueryMoveCost('Obama2012',1,.7)
 
 
 ## User Accounts ##
@@ -82,85 +81,140 @@ def CreateAccount(Name,Qfunds):
     """Creates an account filled with money.
     Obviously, this is a crucial step which will require (!) verification of Bitcoin payments, an X-confirmation delay, etc. For testing we allow unconstrained (free/infinite) cash.
     These accounts have simple toy names, actual accounts will probably be the bitcoin addresses themselves."""
-    Users.update( {Name : Qfunds})
+    Users.update( {Name : {'Cash': Qfunds}})
+    
+CreateAccount('Bob',10)
+CreateAccount('Alice',200)
 
 
 
 
 ## Buying and Selling Shares ##
 
-Buy <- function(uID,ID,State,P,Verbose=TRUE) {
+def Buy(uID,ID,State,P,Verbose=True):
+    """Takes User Id, Market Id, Market State, and Probability Target, and deducts funds from the user such that
+    the relevant market's state is the probability target."""
   
-  #Calculate Required Cost
-  BaseCost <- QueryMoveCost(ID,State,P) #trade cost assuming no fees
-  Fee <- BaseCost*FeeRate               #fees for buying only (global parameter set at top)
-  TotalCost <- BaseCost + Fee           #Total cost including Fee
+    #Calculate Required Cost
+    BaseCost = QueryMoveCost(ID,State,P) #trade cost assuming no fees
+    Fee = BaseCost*FeeRate               #fees for buying only (global parameter set at top)
+    TotalCost = BaseCost + Fee           #Total cost including Fee
 
-  MarginalShares <- QueryMove(ID,State,P)
-  if(MarginalShares<0) return("Price already exceeds target. Sell shares or buy a Mutually Exclusive State (MES).")
-  if(Verbose) { print(paste("Calulating Required Shares...",MarginalShares)); print(paste("Determining Cost of Trade...",BaseCost)); print(paste("Fee:",Fee)) }
-  
-  #Reduce Funds, add Shares
-  if( Users[[uID]]$Cash < TotalCost) return("Insufficient Funds")
-  Users[[uID]]$Cash <<-  Users[[uID]]$Cash - TotalCost 
-  OldShares <- Users[[uID]][[ID]][[paste("State",State,sep="")]] ; if(is.null(OldShares)) OldShares <- 0
-  Users[[uID]][[ID]][[paste("State",State,sep="")]] <<- OldShares + MarginalShares
+    MarginalShares = QueryMove(ID,State,P)
+    if MarginalShares<0:
+        return("Price already exceeds target. Sell shares or buy a Mutually Exclusive State (MES).")
+    if Verbose:
+        print("Calulating Required Shares..." + str(MarginalShares))
+        print("Determining Cost of Trade..." + str(BaseCost))
+        print("Fee: " + str(Fee))
+      
+    #Reduce Funds, add Shares
+    if Users[uID]['Cash'] < TotalCost:
+        return("Insufficient Funds")
+    StateName = 'State ' + str(State) # As far as the user is concerned, State 1 is the first state (not State 0 as python is concerned)
+    Users[uID]['Cash'] =  Users[uID]['Cash'] - TotalCost 
+    #Is there an entry for this already?
+    try:
+        OldShares = Users[uID][ID][StateName]
+        Users[uID][ID][StateName] = OldShares + MarginalShares
+    except KeyError: # (first time buying in this state)
+        OldShares = 0
+        try:
+            Users[uID][ID].update( {StateName: MarginalShares} )
+        except KeyError: # (first time buying in this market)
+            Users[uID].update( {ID: {StateName: MarginalShares}} )
     
-  #Credit Funds, add Shares
-  Markets[[ID]]$Balance <<-  Markets[[ID]]$Balance + BaseCost
-  Markets[[ID]]$FeeBalance <<-  Markets[[ID]]$FeeBalance + Fee
-  Markets[[ID]]$Shares[State] <<- Markets[[ID]]$Shares[State] + MarginalShares  
+    #Credit Funds, add Shares
+    Markets[ID]['Balance'] = Markets[ID]['Balance'] + BaseCost
+    Markets[ID]['FeeBalance'] = Markets[ID]['FeeBalance'] + Fee
+    FlatMarket = copy(Markets[ID]['Shares']).flatten()
+    FlatMarket[(State-1)] = FlatMarket[(State-1)] + MarginalShares
+    Markets[ID]['Shares'] = FlatMarket.reshape( Markets[ID]['Shares'].shape ) #restore original shape
   
-  if(Verbose) print(paste("Bought",MarginalShares,"for",TotalCost,"."))
-  return(c(MarginalShares,TotalCost))
-}
-
-Sell <- function(uID,ID,State,P,Verbose=TRUE) {
-  #Calculate Required Cost
-  Cost <- QueryMoveCost(ID,State,P)
-  MarginalShares <- QueryMove(ID,State,P)
-  if(MarginalShares>0) return("Price already below target. Buy shares or sell a Mutually Exclusive State (MES).")
-  if(Verbose) { print("Calulating Required Shares..."); print(MarginalShares); print("Determining Cost of Trade..."); print(Cost) }
-  
-  #Reduce shares, add Funds
-  OldShares <- Users[[uID]][[ID]][[paste("State",State,sep="")]]
-  if(OldShares<(-1*MarginalShares)) return("Insufficient Shares") #Remember, shares are negative.
-  Users[[uID]][[ID]][[paste("State",State,sep="")]] <<- OldShares + MarginalShares 
-  Users[[uID]]$Cash <<-  Users[[uID]]$Cash - Cost #Cost is also negative
-  
-  #Remove Funds and Shares from Market
-  Markets[[ID]]$Balance <<-  Markets[[ID]]$Balance + Cost  
-  Markets[[ID]]$Shares[State] <<- Markets[[ID]]$Shares[State] + MarginalShares 
-  
-  if(Verbose) print(paste("Sold",-1*MarginalShares,"for",-1*Cost,"."))
-  return(c(MarginalShares,Cost))
-}
-
-## NOT DONE !!!
-
-FinalSell <- function(uID,ID,State,S,Verbose=TRUE) {
-  #This function takes over after the event's state has been determined, and all shares are either worth zero or the unit price.
-  Judged <- BlockChain[[length(BlockChain)]]$Jmatrix
-  ContractState <- -2
-  ContractState <- try(Judged[Judged$Contract==ID,2])
-  if(Verbose) print(paste("Determined State of this Market:", ContractState))
-  
-  #Which shares are valuable?
-  if(ContractState<0) return("You cannot sell using this function until there is a consensus about the outcome.") 
-  if(State!=ContractState) return("Shares of this state have value 0.") 
-  OldShares <- Users[[uID]][[ID]][[paste("State",State,sep="")]]
-  
-  MarginalShares <- S*-1 #Users are expected to enter +3 if they wish to sell 3 shares, ie marginally change shares by -3.
-  Cost <- MarginalShares # All shares have value 1, so this identiy holds.
+    if Verbose:
+        print("Bought " + str(MarginalShares) + " for " + str(TotalCost) + ".")
+    return((MarginalShares,TotalCost))
     
-  #Reduce shares, add Funds
-  Users[[uID]][[ID]][[paste("State",State,sep="")]] <<- OldShares + MarginalShares #MarginalShares are negative
-  Users[[uID]]$Cash <<-  Users[[uID]]$Cash - Cost #Cost is negative
+def Sell(uID,ID,State,P,Verbose=True):
+    """Takes User Id, Market Id, Market State, and Probability Target, and deducts funds from the user such that
+    the relevant market's state is the probability target."""
   
-  #Remove Funds and Shares from Market
-  Markets[[ID]]$Balance <<-  Markets[[ID]]$Balance + Cost  
-  Markets[[ID]]$Shares[State] <<- Markets[[ID]]$Shares[State] + MarginalShares 
+    #Calculate Required Cost
+    BaseCost = QueryMoveCost(ID,State,P) #trade cost assuming no fees
+    TotalCost = BaseCost                 #No fees for selling
+
+    MarginalShares = QueryMove(ID,State,P)
+    if MarginalShares>0:
+        return("Price already exceeds target. Sell shares or buy a Mutually Exclusive State (MES).")
+    if Verbose:
+        print("Calulating Required Shares..." + str(MarginalShares))
+        print("Determining Cost of Trade..." + str(BaseCost))
+      
+    #Reduce Shares, add Funds
+    StateName = 'State ' + str(State) # As far as the user is concerned, State 1 is the first state (not State 0 as python is concerned)  
+    try:
+        OldShares = Users[uID][ID][StateName]
+    except KeyError:
+        return("Zero Shares")
+    if OldShares < (-1*MarginalShares): #Remember, 'marginal shares' are negative when selling.
+        return("Insufficient Shares")
+    Users[uID][ID][StateName] = OldShares + MarginalShares #Reduce Shares (shares are negative)    
+    Users[uID]['Cash'] =  Users[uID]['Cash'] - TotalCost   #Add Funds (costs are negative)
+
+    #Remove Funds and Shares from Market
+    Markets[ID]['Balance'] = Markets[ID]['Balance'] + BaseCost
+    FlatMarket = copy(Markets[ID]['Shares']).flatten()
+    FlatMarket[(State-1)] = FlatMarket[(State-1)] + MarginalShares
+    Markets[ID]['Shares'] = FlatMarket.reshape( Markets[ID]['Shares'].shape ) #restore original shape
   
-  if(Verbose) print(paste("FinalSold",-1*MarginalShares,"for",-1*Cost,"."))
-  return(c(MarginalShares,Cost))
-}  
+    if Verbose:
+        print("Sold " + str(-1*MarginalShares) + " for " + str(-1*TotalCost) + ".")
+    return((MarginalShares,TotalCost))
+
+
+
+Buy('Bob','Obama2012',1,.7,True)
+ShowPrices('Obama2012')
+Sell('Bob','Obama2012',1,.5,True)
+ShowPrices('Obama2012')
+
+Buy('Bob','Obama2012',1,.7,True)
+ShowPrices('Obama2012')
+Buy('Bob','Obama2012',1,.9,True)
+ShowPrices('Obama2012')
+Sell('Bob','Obama2012',1,.5,True)
+ShowPrices('Obama2012')
+
+Sell('Bob','Obama2012',1,.4,True)
+ShowPrices('Obama2012')
+Buy('Bob','Obama2012',1,.9999,True)
+ShowPrices('Obama2012')
+
+### NOT DONE !!!
+#
+#FinalSell <- function(uID,ID,State,S,Verbose=TRUE) {
+#  #This function takes over after the event's state has been determined, and all shares are either worth zero or the unit price.
+#  Judged <- BlockChain[[length(BlockChain)]]$Jmatrix
+#  ContractState <- -2
+#  ContractState <- try(Judged[Judged$Contract==ID,2])
+#  if(Verbose) print(paste("Determined State of this Market:", ContractState))
+#  
+#  #Which shares are valuable?
+#  if(ContractState<0) return("You cannot sell using this function until there is a consensus about the outcome.") 
+#  if(State!=ContractState) return("Shares of this state have value 0.") 
+#  OldShares <- Users[[uID]][[ID]][[paste("State",State,sep="")]]
+#  
+#  MarginalShares <- S*-1 #Users are expected to enter +3 if they wish to sell 3 shares, ie marginally change shares by -3.
+#  Cost <- MarginalShares # All shares have value 1, so this identiy holds.
+#    
+#  #Reduce shares, add Funds
+#  Users[[uID]][[ID]][[paste("State",State,sep="")]] <<- OldShares + MarginalShares #MarginalShares are negative
+#  Users[[uID]]$Cash <<-  Users[[uID]]$Cash - Cost #Cost is negative
+#  
+#  #Remove Funds and Shares from Market
+#  Markets[[ID]]$Balance <<-  Markets[[ID]]$Balance + Cost  
+#  Markets[[ID]]$Shares[State] <<- Markets[[ID]]$Shares[State] + MarginalShares 
+#  
+#  if(Verbose) print(paste("FinalSold",-1*MarginalShares,"for",-1*Cost,"."))
+#  return(c(MarginalShares,Cost))
+#}  
